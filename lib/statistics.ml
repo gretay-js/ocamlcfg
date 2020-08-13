83448728b255b3ea3c2b7c8dc5d5a96d114fd9df
Nandor Licker <n@ndor.email>
2020-08-13 13:56:01 +0100

    avoid writing empty stats files

--- lib/statistics.ml
+++ lib/statistics.ml
@@ -8,16 +8,18 @@ let record ~file f =
   counters := StringMap.empty;
   Misc.try_finally f
     ~always:(fun () ->
-      let oc = open_out file in
-      let fmt = Format.formatter_of_out_channel oc in
-      StringMap.iter
-        (fun group key_val ->
-          StringMap.iter
-            (fun key v -> Format.fprintf fmt "%s.%s: %d\n" group key v)
-            key_val)
-        !counters;
-      close_out oc;
-      counters := old_counters)
+      if not (StringMap.is_empty !counters) then begin
+        let oc = open_out file in
+        let fmt = Format.formatter_of_out_channel oc in
+        StringMap.iter
+          (fun group key_val ->
+            StringMap.iter
+              (fun key v -> Format.fprintf fmt "%s.%s: %d\n" group key v)
+              key_val)
+          !counters;
+        close_out oc;
+        counters := old_counters
+      end)
 
 let set ~group ~key v =
   counters := StringMap.update group (function
